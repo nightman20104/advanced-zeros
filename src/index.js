4e60727a39cd41c1a1ae7b6fc4a690f40b066608
nightman20104 <programist20104@gmail.com>
2018-10-07 23:50:15 -0700

    I've tried

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+let massiv = [];
+let nfs="";
+let ostatok, chastnoe;
+let bufer = number;
+do {
+  ostatok = bufer%base;
+  chastnoe = Math.floor(bufer/base);
+  massiv.push (ostatok);
+  bufer = chastnoe;
+} while(chastnoe>=base);
+  massiv.push (chastnoe);
+  for(let i = massiv.length-1;i>=0;i--){
+    nfs+=massiv[i];
+  }
+let rnfs=Number(nfs);
+
+  let five = 5;
+  let zerosCount = 0;
+  while (rnfs>five) {
+    zerosCount = zerosCount + Math.floor(rnfs/five);
+    five = five*5;
+  }
+  return zerosCount;
 }
